cfe15733e73379a8a625b1477c2368855ba28f44
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-07-27 17:19:26 +0200

    upd

--- src/script.js
+++ src/script.js
@@ -2,3 +2,11 @@ var dynYear = document.querySelector('#year');
 if (dynYear != null) {
     dynYear.innerHTML = new Date().getFullYear();
 }
+
+
+//responsive nav toggler
+const nav = document.querySelector('nav');
+const burger = document.querySelector('nav img');
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
