a461978120aeb38c14c52c5b53efecc2ff842228
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-07-27 22:11:54 +0200

    add gallery

--- src/script.js
+++ src/script.js
@@ -10,4 +10,27 @@ if (nav != null) {
   burger.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
+  document.querySelector('nav img').src = "https://raw.githubusercontent.com/SaracenRhue/myLibrary/main/src/icons/burger.svg";
+}
+
+
+const gallery = document.querySelector('.gallery');
+function generate(path) {
+  const count = Number(gallery.classList[1]) + 1;
+  let imgs = [];
+  for (let i = 1; i < count; i++) {
+    imgs.push(i + '.jpg');
+  }
+  imgs.forEach((item) => {
+    let img = document.createElement('img');
+    img.src = path + item;
+    gallery.appendChild(img);
+  });
+}
+
+if (gallery != null) {
+  generate(gallery.classList[2]);
+
+console.log(gallery.classList[2]);
+
 }
