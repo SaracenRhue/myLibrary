a1bf97a1a976940c5dd7b4b11ed4f0fbb70e5a31
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-07-27 22:11:04 +0200

    add gallery

--- src/script.js
+++ src/script.js
@@ -1,12 +1,13 @@
-var dynYear = document.querySelector('#year');
+const dynYear = document.querySelector('#year');
 if (dynYear != null) {
-    dynYear.innerHTML = new Date().getFullYear();
+  dynYear.innerHTML = new Date().getFullYear();
 }
-
 
 //responsive nav toggler
 const nav = document.querySelector('nav');
 const burger = document.querySelector('nav img');
-burger.addEventListener('click', () => {
+if (nav != null) {
+  burger.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
+}
