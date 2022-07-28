020b5307f018397854dee5d082681b7baacd6cb6
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-07-28 10:35:26 +0200

    add sync-nav

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # myLibrary
 
 ### CSS
-  `<link rel="stylesheet" href="https://raw.githubusercontent.com/SaracenRhue/myLibrary/main/src/style.css">`
+  `<link"href="https://raw.githubusercontent.com/SaracenRhue/myLibrary/main/src/style.css" rel="stylesheet crossorigin="anonymous">`
 ### JS
-  `<script defer src="https://raw.githubusercontent.com/SaracenRhue/myLibrary/main/src/script.js"></script>`
+  `<script defer src="https://raw.githubusercontent.com/SaracenRhue/myLibrary/main/src/script.js" crossorigin="anonymous"></script>`
--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ if (dynYear != null) {
 //responsive nav toggler
 const nav = document.querySelector('nav');
 const burger = document.querySelector('nav img');
-if (nav != null) {
+if (nav != null && burger != null) {
   burger.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
@@ -26,6 +26,38 @@ function generate(path) {
     img.src = path + item;
     gallery.appendChild(img);
   });
+}
+
+
+const syncNav = document.querySelector('.sync-nav');
+function syncNavFun() {
+fetch('../index.html')
+  .then(function (response) {
+    // When the page is loaded convert it to text
+    return response.text();
+  })
+  .then(function (html) {
+    // Initialize the DOM parser
+    var parser = new DOMParser();
+
+    // Parse the text
+    var doc = parser.parseFromString(html, 'text/html');
+
+    // You can now even select part of that html as you would in the regular DOM
+    // Example:
+    // var docArticle = doc.querySelector('article').innerHTML;
+
+    //console.log(doc);
+
+    syncNav.innerHTML = doc.querySelector('.sync-nav').innerHTML
+  })
+  .catch(function (err) {
+    console.log('Failed to fetch page: ', err);
+  });
+}
+
+if (nav != null) {
+  syncNavFun();
 }
 
 if (gallery != null) {
